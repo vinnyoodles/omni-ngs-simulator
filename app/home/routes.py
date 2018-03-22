df01ae55130f4575647a7ebae61f3655d8bf4286
Vincent Le <vinnyoodles@gmail.com>
2018-03-22 10:12:46 -0400

    Add card layout and template

--- app/home/routes.py
+++ app/home/routes.py
@@ -3,6 +3,9 @@ from app.home import bp
 from app.models import User
 
 @bp.route('/')
-@bp.route('/index')
 def index():
     return render_template('index.html', title='Home', transparent_nav=True)
+
+@bp.route('/simulators')
+def simulators():
+    return render_template('cards.html', title='Cards')

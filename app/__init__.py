92c07f74eef8cf33833f8d870ad9100eeaaa325b
Vincent Le <vinnyoodles@gmail.com>
2018-05-01 18:59:44 -0400

    Add url prefix

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,14 @@ from flask_login import LoginManager
 from config import Config
 from mongoengine import connect
 from celery import Celery
+from werkzeug.routing import Rule
 
-instance = Flask(__name__, static_url_path='/static')
+ROUTE_PREFIX = '/prefix'
+
+instance = Flask(__name__, static_url_path=ROUTE_PREFIX + '/static')
 instance.config.from_object(Config)
+instance.url_rule_class = lambda path, **options: Rule(ROUTE_PREFIX + path, **options)
+
 
 login = LoginManager(instance)
 login.login_message = 'Please log in to access this page.'

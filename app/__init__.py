844a963a014c2dcce58091edb3943f9a622cf994
Vincent Le <vinnyoodles@gmail.com>
2018-05-11 18:41:12 -0400

    Fix double url prefix

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,8 @@ from mongoengine import connect
 from celery import Celery
 from werkzeug.routing import Rule
 
-PROD_ENV = os.environ.get('PROD_ENV') == '1'
-
-ROUTE_PREFIX = ''
-if PROD_ENV:
-    ROUTE_PREFIX = '/omningssimulator'
-
 instance = Flask(__name__, static_url_path='/static')
 instance.config.from_object(Config)
-instance.url_rule_class = lambda path, **options: Rule(ROUTE_PREFIX + path, **options)
 
 login = LoginManager(instance)
 login.login_message = 'Please log in to access this page.'

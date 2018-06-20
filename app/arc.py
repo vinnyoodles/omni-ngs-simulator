5feaa8a67a6f6d35e9772a9338aa65d712578176
Usman Ahmed <usmana@vt.edu>
2018-06-19 21:36:07 -0400

    Update arc.py

--- app/arc.py
+++ app/arc.py
@@ -1,7 +1,7 @@
 import paramiko
 from scp import SCPClient
 
-ARC_USER = 'usmana'
+ARC_USER = 'vincent1'
 ARC_DIR = '/groups/ngsproj/generated_data'
 QSUB_DIR = '/groups/ngsproj/omni-ngs-simulator/arc'
 
